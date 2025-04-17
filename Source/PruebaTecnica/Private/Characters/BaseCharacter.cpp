791ba94e7506fa91a5eff56e2b2a00440a56cfd5
IkerNoya <ikernoya4@gmail.com>
2025-04-17 20:47:36 -0300

    added new locomotion and animations

--- Source/PruebaTecnica/Private/Characters/BaseCharacter.cpp
+++ Source/PruebaTecnica/Private/Characters/BaseCharacter.cpp
@@ -3,10 +3,25 @@
 
 #include "PruebaTecnica/Public/Characters/BaseCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputSubsystems.h"
+
 
 ABaseCharacter::ABaseCharacter()
 {
 	PrimaryActorTick.bCanEverTick = true;
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	SpringArmComponent->SetupAttachment(GetCapsuleComponent());
+	SpringArmComponent->TargetArmLength = 400.0f;
+	SpringArmComponent->bUsePawnControlRotation = true;
+	
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->SetupAttachment(SpringArmComponent);
+	CameraComponent->bUsePawnControlRotation = false;
 }
 
 void ABaseCharacter::BeginPlay()
@@ -15,13 +30,78 @@ void ABaseCharacter::BeginPlay()
 	
 }
 
-void ABaseCharacter::Tick(float DeltaTime)
+void ABaseCharacter::PossessedBy(AController* NewController)
 {
-	Super::Tick(DeltaTime);
+	Super::PossessedBy(NewController);
+	//Add mapping context to PlayerController
+	if (APlayerController* PlayerController = Cast<APlayerController>(NewController))
+	{
+		if(UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(MappingContext, 0);
+		}
+	}	
+}
+
+void ABaseCharacter::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D MoveValue = InputActionValue.Get<FVector2D>();
+	const FRotator RotationValue(0.0f, Controller->GetControlRotation().Yaw, 0.0f);
+
+	if (!FMath::IsNearlyZero(MoveValue.X, 0.05f))
+	{
+		const FVector Direction = RotationValue.RotateVector(FVector::RightVector);
+		AddMovementInput(Direction, MoveValue.X);
+	}
+
+	if (!FMath::IsNearlyZero(MoveValue.Y, 0.05f))
+	{
+		const FVector Direction = RotationValue.RotateVector(FVector::ForwardVector);
+		AddMovementInput(Direction, MoveValue.Y);
+	}
+}
+
+void ABaseCharacter::Look(const FInputActionValue& InputActionValue)
+{
+	const FVector2D LookValue = InputActionValue.Get<FVector2D>();
+
+	if (!FMath::IsNearlyZero(LookValue.X, 0.05f))
+	{
+		AddControllerYawInput(LookValue.X);
+	}
+
+	if (!FMath::IsNearlyZero(LookValue.Y, 0.05f))
+	{
+		AddControllerPitchInput(LookValue.Y);
+	}
+}
+
+void ABaseCharacter::Interact()
+{
+	//TODO: Handle interaction
+}
+
+void ABaseCharacter::ToggleWalk(const FInputActionValue& InputActionValue)
+{
+	const bool bIsWalking = InputActionValue.Get<bool>();
+	const float WalkSpeed = bIsWalking ? 250.0f : 500.0f;
+	GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
 }
 
 void ABaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+{	
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveInputAction, ETriggerEvent::Triggered, this, &ABaseCharacter::Move);
+	
+		EnhancedInputComponent->BindAction(LookInputAction, ETriggerEvent::Triggered, this, &ABaseCharacter::Look);
+	
+		EnhancedInputComponent->BindAction(JumpInputAction, ETriggerEvent::Triggered, this, &ABaseCharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpInputAction, ETriggerEvent::Completed, this, &ABaseCharacter::StopJumping);
+	
+		EnhancedInputComponent->BindAction(InteractInputAction, ETriggerEvent::Triggered, this, &ABaseCharacter::Interact);
+	
+		EnhancedInputComponent->BindAction(ToggleWalkInputAction, ETriggerEvent::Triggered, this, &ABaseCharacter::ToggleWalk);
+	}
 }
 
--- Source/PruebaTecnica/Public/Characters/BaseCharacter.h
+++ Source/PruebaTecnica/Public/Characters/BaseCharacter.h
@@ -4,7 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "EnhancedInput/Public/EnhancedInputComponent.h"
+#include "InputMappingContext.h"
 #include "BaseCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
 
 UCLASS()
 class PRUEBATECNICA_API ABaseCharacter : public ACharacter
@@ -14,11 +19,40 @@ class PRUEBATECNICA_API ABaseCharacter : public ACharacter
 public:
 	ABaseCharacter();
 
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
 protected:
 	virtual void BeginPlay() override;
 
-public:
-	virtual void Tick(float DeltaTime) override;
+	virtual void PossessedBy(AController* NewController) override;
 
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	void Move(const FInputActionValue& InputActionValue);
+	void Look(const FInputActionValue& InputActionValue);
+	void ToggleWalk(const FInputActionValue& InputActionValue);
+	
+	void Interact();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	UCameraComponent* CameraComponent = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	USpringArmComponent* SpringArmComponent = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputMappingContext* MappingContext = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* JumpInputAction = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* InteractInputAction = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* MoveInputAction = nullptr;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* LookInputAction = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* ToggleWalkInputAction = nullptr;
 };
